6decdc8581ed45b73ece6832633035014cb57ea3
user <sofiia.bykovska@chisw.com>
2023-08-23 16:50:52 +0300

    Made a map and put main character to game

---
+++ lib/games/green_ninja_game.dart
@@ -0,0 +1,28 @@
+import 'package:bonfire/bonfire.dart';
+import 'package:flutter/material.dart';
+import 'package:green_ninja_flame/constants/globals.dart';
+import 'package:green_ninja_flame/players/green_ninja_player.dart';
+import 'package:green_ninja_flame/sprite_sheets/green_ninja_sprite_sheet.dart';
+
+class GreenNinjaGame extends StatefulWidget {
+  const GreenNinjaGame({Key? key}) : super(key: key);
+
+  @override
+  State<GreenNinjaGame> createState() => _GreenNinjaGameState();
+}
+
+class _GreenNinjaGameState extends State<GreenNinjaGame> {
+  @override
+  Widget build(BuildContext context) {
+    return BonfireWidget(
+      joystick: Joystick(
+        directional: JoystickDirectional()
+      ),
+      player: GreenNinjaPlayer(position: Vector2(40, 40), spriteSheet: GreenNinjaSpriteSheet.spriteSheet),
+      map: WorldMapByTiled(
+        Globals.mapOne,
+        forceTileSize: Vector2(32, 32),
+      ),
+    );
+  }
+}
---
+++ lib/players/green_ninja_player.dart
@@ -0,0 +1,17 @@
+import 'package:bonfire/bonfire.dart';
+import 'package:green_ninja_flame/constants/animation_configs.dart';
+import 'package:green_ninja_flame/constants/globals.dart';
+
+class GreenNinjaPlayer extends SimplePlayer {
+  GreenNinjaPlayer(
+      {required Vector2 position, required SpriteSheet spriteSheet})
+      : super(
+          size: Vector2(Globals.playerSize, Globals.playerSize),
+          position: position,
+          speed: 100,
+          life: 100,
+          initDirection: Direction.down,
+          animation:
+              AnimationConfigs.greenNinjaAnimation(spriteSheet: spriteSheet),
+        );
+}
